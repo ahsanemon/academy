ad84e45940f7a5558c20d5784d14d05ae894af44
Ahsan Ullah Ahmad <a.ahmad@cognigy.com>
2024-10-01 18:07:10 +0200

    End of day 06 (few small things are missing)

---
+++ 06-vue-playground/counter/src/App.vue
@@ -0,0 +1,20 @@
+<script  setup>
+  import CounterDisplay from './components/CounterDisplay.vue';
+</script>
+
+<template>
+  <button @click="number--">-</button>
+  <!-- <CounterDisplay v-bind:number="number"/> -->
+  <CounterDisplay :number="number"/>
+  <button @click="number++">+</button>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        number: 1,
+      };
+    },
+  }
+</script>
---
+++ 06-vue-playground/counter/src/components/CounterDisplay.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    {{ number }} {{ sticks }}
+  </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            number: {
+                required: true,
+                // default: 0,
+                type: Number,
+            }
+        },
+        computed: {
+            sticks () {
+                let out = '';
+                for (let i = 0; i < this.number; i++) {
+                    out += ' | ';  
+                }
+
+                return out;
+            }
+        },
+    }
+</script>
