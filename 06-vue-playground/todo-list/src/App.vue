efdb8635d9d0b2d08ed0fa195954a2c7ea11f12f
Ahsan Ullah Ahmad <a.ahmad@cognigy.com>
2024-10-15 18:07:27 +0200

    class end (with missing things)

---
+++ 06-vue-playground/todo-list/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+  import TodoList from './components/TodoList.vue';
+</script>
+
+<template>
+  <h1> hello</h1>
+  <TodoList />
+</template>
+
+<style scoped>
+
+</style>
---
+++ 06-vue-playground/todo-list/src/components/TodoList.vue
@@ -0,0 +1,64 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="item in items">
+                {{ item.text }}
+            </li>
+        </ul>
+
+        {{ addText }}
+        
+        <form @submit.prevent="onAdd">
+            <input type="text" v-model="addText"/>
+            <button type="submit">Add</button>
+        </form>
+    </div>
+</template>
+
+<script>
+
+    const StatusEnum = Object.freeze({
+        pending: 'pending', 
+        done: 'done', 
+        canceled: 'canceled',
+    });
+
+    export default {
+        data () {
+            return {
+                addText: '',
+                items: [
+                    {
+                        text: 'Do this',
+                        status: StatusEnum.pending,
+                        archived: false,
+                        due: null,
+                        color: 'green',
+                    },
+                    {
+                        text: 'Do that',
+                        status: StatusEnum.pending,
+                        archived: false,
+                        due: null,
+                        color: 'green',
+                    },
+                    {
+                        text: 'Do this third thing',
+                        status: StatusEnum.pending,
+                        archived: false,
+                        due: null,
+                        color: 'green',
+                    },
+                ],
+            };
+        
+        },
+        methods: {
+            onAdd () {
+                this.items.push({
+                    text: this.addText,
+                })
+            },
+        }
+    }
+</script>
